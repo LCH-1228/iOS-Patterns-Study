85b8b91612475b100ccc9c5dec092a3088eea8aa
LCH-1228 <chanho1228@gmail.com>
2025-08-25 14:59:23 +0900

    ImageCacheManager 기능 구현

---
+++ MVVM/MVVM/Service/ImageCacheManager/ImageCacheManager.swift
@@ -0,0 +1,29 @@
+//
+//  ImageCacheManager.swift
+//  MVVM
+//
+//  Created by LCH on 8/25/25.
+//
+
+import Foundation
+
+final class ImageCacheManager {
+    static let shared = ImageCacheManager()
+    private let cache = NSCache<NSNumber, NSData>()
+    
+    private init() {}
+    
+    func setImage(_ data: Data, forKey key: Int) {
+        let nsData = NSData(data: data)
+        let nsKey = NSNumber(value: key)
+        cache.setObject(nsData, forKey: nsKey)
+    }
+    
+    func getImage(forKey key: Int) -> Data? {
+        let nsKey = NSNumber(value: key)
+        if let nsData = cache.object(forKey: nsKey) {
+            return Data(referencing: nsData)
+        }
+        return nil
+    }
+}
